6a677c363f068fb0d5fde9135c864d19a7dbdf90
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-08 00:16:00 +0530

    creating article form user functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "pages/home"
   root "pages#home"
   get "pages/about", to: "pages#about"
-  resources :articles # ,only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 end
