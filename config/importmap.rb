530de14643b9b129d31e2ccb5fad8b9344baac91
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-10 02:01:34 +0530

    added delete function for articles

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "@rails/ujs", to: "rails-ujs.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "controllers", to: "controllers.js"
+pin "@rails/ujs", to: "rails-ujs.js"
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "pages/home"
   root "pages#home"
   get "pages/about", to: "pages#about"
+
   resources :articles # , only: [:show, :index, :new, :create]
 end
