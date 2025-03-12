1a3e0d8ae0ff2a739c388bd69a505da5b2793779
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-13 02:06:06 +0530

    form edited added styling and alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice then "alert-success"  # Green for success messages
+    when :alert then "alert-danger"    # Red for error messages
+    else "alert-info"                  # Default (Blue)
+    end
+  end
 end
