667e42fea7c37c68de4032e82577c42819abf77f
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-17 01:56:16 +0530

    added user edit templete and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account has been updated successfully."
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
 
   private
 
