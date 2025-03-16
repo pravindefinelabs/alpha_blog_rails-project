2bd89f15584659b5a224ce33c92abf26a43d049e
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-16 23:52:37 +0530

    Added user create action from home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You have successfully signed up!"
+      flash[:notice] = "Hello #{@user[:username]} , Welcome to Alpha Blog."
       redirect_to articles_path
     else
       render "new"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
