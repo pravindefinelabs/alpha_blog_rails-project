e8c447da17f35f90af8866cae39ff7bff6dea534
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-19 01:23:56 +0530

    added controller level Restriction and user delete action with delete all article of that user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update,:destroy]
+
   def new
     @user = User.new
   end
@@ -14,10 +18,10 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account has been updated successfully."
       redirect_to @user
@@ -33,9 +37,27 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id]= nil
+    flash[:notice] ="Account and all associated articles successfully deleted"
+    redirect_to root_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account."
+      redirect_to @user
+    end
   end
 end
