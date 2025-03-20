bcb07fc72223196209c901be867e2387cec376f9
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-20 20:49:17 +0530

    added caterory edit action and showing all article to associate category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,13 +15,25 @@ class CategoriesController <ApplicationController
       render 'new'
     end
   end
-  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Your category has been updated successfully."
+      redirect_to @category
+    else
+      render "edit"
+    end
+  end
   def index
    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
 private 
